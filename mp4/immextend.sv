5e0dcc9414ec9d0f00b7a07ab2b39a19b16db2fd
Amanda Chang <achang2@olin.edu>
2025-11-20 16:21:41 -0500

    updating immediate extend to match instruction decoder

--- mp4/immextend.sv
+++ mp4/immextend.sv
@@ -1,24 +1,25 @@
-module extend (
+// Immediate Extender
+//
+module imm_extend (
     input [31:0] instr,
+    input [2:0] imm_src,
     output [31:0] imm_ext
 );
     // 7 bit opcodes decoded
-    localparam [6:0] UTYPE  = 7'b0110111;
-    localparam [6:0] ITYPEL = 7'b0010011;
-    localparam [6:0] ITYPEA = 7'b0000011;
-    localparam [6:0] STYPE  = 7'b0100011;
-    localparam [6:0] BTYPE  = 7'b1100011;
-    localparam [6:0] JTYPE  = 7'b1101111;
-    localparam [6:0] RTYPE  = 7'b0110011;
+    localparam [2:0] ITYPE = 3'b000;
+    localparam [2:0] STYPE = 3'b001;
+    localparam [2:0] BTYPE = 3'b010;
+    localparam [2:0] JTYPE = 3'b011;
+    localparam [2:0] UTYPE = 3'b100;
+    localparam [2:0] RTYPE = 3'b111;
 
     always_comb begin
-        case (instr[6:0]) // read the opcode
-            UTYPE: imm_ext <= {instr[31:12], {12{1'b0}}};
-            ITYPEL: imm_ext <= {{20{instr[31]}}, instr[31:20]};
-            ITYPEA: imm_ext <= {{27{1'b0}}, instr[24:20]};
+        case (imm_src) // read the opcode
+            ITYPE: imm_ext <= {{20{instr[31]}}, instr[31:20]};
             STYPE:  imm_ext <= {{20{instr[31]}}, instr[31:25], instr[11:7]};
             BTYPE:  imm_ext <= {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
             JTYPE:  imm_ext <= {{12{instr[31]}}, instr[19:12], instr[11], instr[30:20], 1'b0};
+            UTYPE: imm_ext <= {instr[31:12], {12{1'b0}}};
             RTYPE:  imm_ext <= {32{1'b0}};
             default:
         endcase
