76a386f12bfd6c60d78d4111ca24c63267d57796
coxlaurel <coxlaurel@gmail.com>
2025-11-21 16:20:38 -0500

    branching instructions

--- mp4/alu.sv
+++ mp4/alu.sv
@@ -7,11 +7,12 @@ References
 */
 
 module alu(
-    input [2:0] alu_control,
-    input [0:31] scr_a,
-    input [0:31] src_b,
-    output [0:31] alu_result,
-    output zero
+    input logic [2:0] alu_control,
+    input logic alu_op,
+    input logic [0:31] scr_a,
+    input logic [0:31] src_b,
+    output logic [0:31] alu_result,
+    output logic zero
 );
 
 // ALUControl is 000 for addition, 001 for subtraction, 010 for AND, 011 for OR, and 101 for set less than.
@@ -21,21 +22,48 @@ localparam AND = 3'b010;
 localparam OR = 3'b011;
 localparam SLT = 3'b101; // SET_LESS_THAN
 
+// Branching functions if alu_op is true (1)
+localparam BEQ = 3'b000; // equal
+localparam BNE = 3'b001; // not equal
+localparam BLT = 3'b100; // less than
+localparam BGTOE = 3'b101; // greater than or equal
+localparam BLTU = 3'b110; // less than unsigned
+localparam BGTOEU = 3'b111; // greater than or equal unsigned
+
+
 logic [0:31] alu_result;
 logic zero;
+logic alu_op;
 
 always_comb begin
     case (alu_control):
-        ADD:
-            alu_result = scr_a + src_b;
-        SUB:
-            alu_result = scr_a - src_b;
-        AND: 
-            alu_result = scr_a & src_b; // HELP
-        OR:
-            alu_result = scr_a | src_b; // HELP
-        SLT:
-            alu_result = scr_a < src_b; // HELP
+        if (alu_op) begin
+            // add BRANCHING FUNCTIONS
+            BEQ:
+                alu_result = (scr_a == src_b);
+            BNE:
+                alu_result = (scr_a != src_b);
+            BLT:
+                alu_result = (scr_a <= src_b);
+            BGTOE:
+                alu_result = ((src_a > src_b) | (scr_a == src_b));
+            BLTU:
+                alu_result = // HELP WHA UNSIGNED????!!!
+            BGTOEU:
+                alu_result = // HELPPPP
+
+        end else begin
+            ADD:
+                alu_result = scr_a + src_b;
+            SUB:
+                alu_result = scr_a - src_b;
+            AND: 
+                alu_result = (scr_a & src_b); // HELP
+            OR:
+                alu_result = (scr_a | src_b); // HELP
+            SLT:
+                alu_result = (scr_a < src_b); // HELP
+        end
 
         default: alu_result = 32'b0;
     endcase
