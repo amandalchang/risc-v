ef39b2a68c21af4fc4b9a6f0625b868d80c10d85
Amanda Chang <achang2@olin.edu>
2025-12-01 16:10:57 -0500

    fixing syntax

--- mp4/control.sv
+++ mp4/control.sv
@@ -9,7 +9,8 @@ module control(
     output logic    pc_write,
     output logic    adr_src,
     output logic    mem_write,
-    output logic    ir_write
+    output logic    ir_write,
+    input logic     clk,
     input [31:0]    instr,
     input logic     zero,
     output logic    [1:0] result_src,
@@ -17,20 +18,26 @@ module control(
     output logic    [1:0] alu_src_b,
     output logic    [1:0] alu_src_a,
     output logic    [1:0] imm_src,
-    output logic    [1:0] reg_write
+    output logic    reg_write
 );
 
-    localparam opcode = instr[6:0];
-    localparam funct3 = instr[14:12];
-    localparam funct7 = instr[31:25];
+    logic [6:0] opcode;
+    logic [2:0] funct3;
+    logic [6:0] funct7;
+
+    assign opcode = instr[6:0];
+    assign funct3 = instr[14:12];
+    assign funct7 = instr[31:25];
+
     logic pc_update = 1'b0;
     logic branch = 1'b0;
+    logic [1:0] alu_op;
     // State variables; naming is defined by starting color
     localparam [2:0] FETCH = 3'b000;
     localparam [2:0] DECODE = 3'b001;
 
     localparam [2:0] EXECUTE = 3'b010;
-    localparam [6:0] MEMADR = 7'b0x00011;
+    localparam [6:0] MEMADR = 7'b0_00011;
     localparam [6:0] EXECUTE_R = 7'b0110011;
     localparam [6:0] EXECUTE_L = 7'b0010011;
     localparam [6:0] JAL = 7'b1101111;
@@ -52,109 +59,120 @@ module control(
     localparam [6:0] MEMWB = MEMREAD;
     
     // Declare state variables
-    logic [3:0] current_state = FETCH;
+    logic [2:0] current_state = FETCH;
+
+    always_comb @(posedge clk) begin
+        if ((zero && branch) | pc_update) begin
+            pc_write = 1'b1;
+        end
+    end
 
     always_ff @(posedge clk) begin
         // if it's time to increment
         case (current_state) // increment based on state
             FETCH: begin
                 // reset enables
-                reg_write = 1'b0;
-                mem_write = 1'b0;
-                branch = 1'b0;
+                reg_write <= 1'b0;
+                mem_write <= 1'b0;
+                branch <= 1'b0;
                 // state logic
-                adr_src = 1'b0;
-                ir_write = 1'b1;
-                alu_src_a = 2'b00; // pc
-                alu_src_b = 2'b10; // 4
-                alu_op = 2'b00; // ADD
-                result_src = 2'b10; // from ALUResult
-                pc_update = 1'b1; // prep for jump/branch
+                adr_src <= 1'b0;
+                ir_write <= 1'b1;
+                alu_src_a <= 2'b00; // pc
+                alu_src_b <= 2'b10; // 4
+                alu_op <= 2'b00; // ADD
+                result_src <= 2'b10; // from ALUResult
+                pc_update <= 1'b1; // prep for jump/branch
             end
             DECODE: begin
                 // reset enables
-                ir_write = 1'b0;
-                pc_update = 1'b0;
+                ir_write <= 1'b0;
+                pc_update <= 1'b0;
                 // state logic
-                alu_src_a = 2'b01;
-                alu_src_b = 2'b01;
-                alu_op = 2'b00;
+                alu_src_a <= 2'b01;
+                alu_src_b <= 2'b01;
+                alu_op <= 2'b00;
             end
             EXECUTE: begin
             case(opcode)
                 MEMADR: begin
                     // state logic
-                    alu_src_a = 2'b10;
-                    alu_src_b = 2'b01;
-                    alu_op = 2'b00;
+                    alu_src_a <= 2'b10;
+                    alu_src_b <= 2'b01;
+                    alu_op <= 2'b00;
                 end
                 EXECUTE_R: begin
                     // state logic
-                    alu_src_a = 2'b10;
-                    alu_src_b = 2'b00;
-                    alu_op = 2'b10;
+                    alu_src_a <= 2'b10;
+                    alu_src_b <= 2'b00;
+                    alu_op <= 2'b10;
                 end
                 EXECUTE_L: begin
                     // state logic
-                    alu_src_a = 2'b10;
-                    alu_src_b = 2'b01;
-                    alu_op = 2'b10;
+                    alu_src_a <= 2'b10;
+                    alu_src_b <= 2'b01;
+                    alu_op <= 2'b10;
                 end
                 JAL: begin
                     // state logic
-                    alu_src_a = 2'b01;
-                    alu_src_b = 2'b10;
-                    alu_op = 2'b00;
-                    result_src = 2'b00;
-                    pc_update = 1'b1;
+                    alu_src_a <= 2'b01;
+                    alu_src_b <= 2'b10;
+                    alu_op <= 2'b00;
+                    result_src <= 2'b00;
+                    pc_update <= 1'b1;
                 end
                 BRANCH: begin
                     // state logic
-                    alu_src_a = 2'b10;
-                    alu_src_b = 2'b00;
-                    alu_op = 2'b01;
-                    result_src = 2'b00;
-                    branch = 1'b1;
+                    alu_src_a <= 2'b10;
+                    alu_src_b <= 2'b00;
+                    alu_op <= 2'b01;
+                    result_src <= 2'b00;
+                    branch <= 1'b1;
                 end
             endcase
             end
             MEMORY: begin
             case(opcode)
                 MEMREAD: begin
-                    result_src = 2'b00;
-                    adr_src = 1'b1;
+                    result_src <= 2'b00;
+                    adr_src <= 1'b1;
                 end
                 MEMWRITE: begin
-                    result_src = 2'b00;
-                    adr_src = 1'b1;
-                    mem_write = 1'b1;
+                    result_src <= 2'b00;
+                    adr_src <= 1'b1;
+                    mem_write <= 1'b1;
                 end
                 ALUWB: begin
-                    result_src = 2'b00;
-                    reg_write = 1'b1;
+                    result_src <= 2'b00;
+                    reg_write <= 1'b1;
                 end
             endcase
             end
             WRITE_BACK: begin
             case(opcode)
-                MEMWB:
-                    result_src = 2'b01;
-                    reg_write = 1'b1;
-                default:
-                    continue
+                MEMWB: begin
+                    result_src <= 2'b01;
+                    reg_write <= 1'b1;
+                end
+                default: begin
+                    // do nothing
+                end
             endcase
             end
         endcase
-        if (current_state==WRITE_BACK) begin
-            current_state = FETCH;
-        end else current_state += 1'b1;
+
+        if (current_state == WRITE_BACK) begin
+            current_state <= FETCH;
+        end else begin
+            current_state <= current_state + 1'b1;
+        end
     end
 
     ALU_decoder u0 (
         .clk            (clk), 
         .funct3         (funct3),
-        .op_5           (opcode),
-        .funct7_5       (funct7),
+        .op_5           (opcode[5]),
+        .funct7_5       (funct7[5]),
         .alu_op         (alu_op),
         .alu_control    (alu_control)
 
