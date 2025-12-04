8e4517aac0e93af3f4a174cb694e539a157760bc
chillybeans-23 <78663146+chillybeans-23@users.noreply.github.com>
2025-12-03 19:56:26 -0500

    make file

--- mp4/immextend.sv
+++ mp4/immextend.sv
@@ -15,7 +15,7 @@ module imm_extend (
 
     always_comb begin
         case (imm_src) // read the opcode
-            ITYPE: imm_ext <= {{20{instr[31]}}, instr[31:20]};
+            ITYPE: imm_ext <= {{20{instr[31]}}, instr[31:20]}; //sign extend imm so it can be added to rs1 to get memory address
             STYPE:  imm_ext <= {{20{instr[31]}}, instr[31:25], instr[11:7]};
             BTYPE:  imm_ext <= {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
             JTYPE:  imm_ext <= {{12{instr[31]}}, instr[19:12], instr[11], instr[30:20], 1'b0};
