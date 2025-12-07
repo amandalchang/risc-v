500dc895adb0f2f7784da760e9e18bf6939dc402
Amanda Chang <achang2@olin.edu>
2025-12-07 13:02:44 -0500

    replacing always block with continuous assignment

--- mp4/immextend.sv
+++ mp4/immextend.sv
@@ -11,20 +11,21 @@ module imm_extend (
     localparam [2:0] BTYPE = 3'b010;
     localparam [2:0] JTYPE = 3'b011;
     localparam [2:0] UTYPE = 3'b100;
-    localparam [2:0] RTYPE = 3'b111;
 
-    always_comb begin
-        case (imm_src) // read the opcode
-            ITYPE: imm_ext = {{20{instr[31]}}, instr[31:20]}; //sign extend imm so it can be added to rs1 to get memory address
-            STYPE:  imm_ext = {{20{instr[31]}}, instr[31:25], instr[11:7]};
-            BTYPE:  imm_ext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
-            JTYPE:  imm_ext = {{12{instr[31]}}, instr[19:12], instr[11], instr[30:20], 1'b0};
-            UTYPE: imm_ext = {instr[31:12], {12{1'b0}}};
-            RTYPE:  imm_ext = {32{1'b0}};
-           // default: $error("Error: Unexpected imm_src %b detected!", imm_src);
-        endcase
-        //$display("%b", imm_ext);
-    end
+    // initializing precomputed wires for continuous assignment
+    wire [31:0] imm_i = {{20{instr[31]}}, instr[31:20]};
+    wire [31:0] imm_s = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+    wire [31:0] imm_b = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+    wire [31:0] imm_j = {{12{instr[31]}}, instr[19:12], instr[11], instr[30:20], 1'b0};
+    wire [31:0] imm_u = {instr[31:12], 12'b0};
+
+    // select immediate using continuous assignment
+    assign imm_ext = (imm_src == ITYPE) ? imm_i :
+                     (imm_src == STYPE) ? imm_s :
+                     (imm_src == BTYPE) ? imm_b :
+                     (imm_src == JTYPE) ? imm_j :
+                     (imm_src == UTYPE) ? imm_u :
+                     32'b0;  // default for RTYPE or unknown
 
 endmodule
 
